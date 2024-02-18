f6ad073ad1a0aaacb5d8a9e4cb78c8a3108360a4
Climax Mba <climaxium150@gmail.com>
2024-02-18 16:19:42 +0100

    Set up tests

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+});
