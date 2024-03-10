2fc825f3baf343c03e697ec8bfaa7ff29d7192f0
Climax Mba <climaxium150@gmail.com>
2024-03-10 22:03:17 +0100

    Write login & signup

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,92 @@
+import Button from "../components/Button";
+
+function SignUp() {
+  return <form action="#">
+    <div>
+      <label htmlFor="signupName">Name:</label>
+      <input type="text" name="signupName" id="signupName" />
+    </div>
+    
+    <div>
+      <label htmlFor="signupEmail">Email</label>
+      <input type="email" name="signupEmail" id="signupEmail" />
+    </div>
+    
+    <div>
+      <label htmlFor="signupPassword">Password</label>
+      <input type="password" name="signupPassword" id="signupPassword" />
+    </div>
+    
+    <div>
+      <label htmlFor="signupPhone">Phone:</label>
+      <input type="password" name="signupPhone" id="signupPhone" />
+    </div>
+    
+    <div>
+      <label htmlFor="signupAddress">Address:</label>
+      <input type="password" name="signupAddress" id="signupAddress" />
+    </div>
+    
+    <Button type="submit" text="Sign Up" />
+  </form>
+}
+
+function LogIn() {
+  return <form action="#">
+    <div>
+      <label htmlFor="loginEmail">Email:</label>
+      <input type="email" name="loginEmail" id="loginEmail" />
+    </div>
+    
+    <div>
+      <label htmlFor="loginPassword">Password:</label>
+      <input type="password" name="loginPassword" id="loginPassword" />
+    </div>
+
+    <Button type="submit" text="Log In" />
+  </form>
+}
+
+// function Auth() {
+//   return <div>
+//     <div>
+//       <span>Sign In</span>
+//       <span>Sign Up</span>
+//     </div>
+
+//     <div>
+//       <SignUp />
+//       <LogIn />
+//     </div>
+//   </div>
+// }
+
 function Account() {
-  return <></>;
+  return (
+    <div>
+      <h1>Account</h1>
+
+      <div>
+        <div>Name</div>
+        <div>{"Name"}</div>
+      </div>
+
+      <div>
+        <div>Email</div>
+        <div>{"Email"}</div>
+      </div>
+
+      <div>
+        <div>Phone number</div>
+        <div>{"Phone"}</div>
+      </div>
+
+      <div>
+        <div>Address</div>
+        <div>{"Address"}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Account;
