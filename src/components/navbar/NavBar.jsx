78587704d4390d6b6cb6fe0f36791d73524e8130
Climax Mba <climaxium150@gmail.com>
2024-03-17 22:47:01 +0100

    Add navbar breakpoint

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,7 +3,7 @@ import styles from "./navbar.module.css";
 export default function NavBar() {
   return (
     <nav className={styles.navBar}>
-      <span>Menu</span>
+      <span className={styles.menuIcon}>Menu</span>
       <span>Store Logo</span>
       <span>Cart</span>
     </nav>
