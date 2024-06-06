e6d3211eccd7bb6ed3c830325501c5614163c30f
Climax Mba <climaxium150@gmail.com>
2024-06-06 05:56:57 +0100

    Integrate API in categories component

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,33 +1,75 @@
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import ProductsList from "../productsList/ProductsList";
 import ShopItem from "../../components/shopItem/ShopItem";
+import storeAPI from "../../modules/storeAPI";
 
 import styles from "./categories.module.scss";
 
 function CategoryList() {
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setCategories(await storeAPI.getCategories());
+      setIsLoading(false);
+    })();
+  }, []);
+
   return (
-    <ul className={styles.categoryList}>
-      <li><Link to="clothes">Clothes</Link></li>
-      <li><Link to="jewelries">Jewelries</Link></li>
-      <li><Link to="items">Items</Link></li>
-    </ul>
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className={styles.categoryList}>
+          {categories.map((category) => (
+            <li key={category}>
+              <Link to={category}>{category}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
 function Categories() {
   const { category } = useParams();
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setProducts(await storeAPI.getProductsByCategory(category));
+      setIsLoading(false);
+    })();
+  }, [category]);
+
   return (
     <div className={styles.container}>
-      <h2 className={styles.heading}>Categories {category && `| ${category}`}</h2>
+      <h2 className={styles.heading}>
+        Categories {category && `| ${category}`}
+      </h2>
       {category ? (
-        <ProductsList>
-          <ShopItem />
-          <ShopItem />
-          <ShopItem />
-          <ShopItem />
-          <ShopItem />
-          <ShopItem />
-        </ProductsList>
+        <>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ProductsList>
+              {products.map((product) => (
+                <ShopItem
+                  key={product.id}
+                  id={product.id}
+                  title={product.title}
+                  price={product.price}
+                  image={product.image}
+                />
+              ))}
+            </ProductsList>
+          )}
+        </>
       ) : (
         <CategoryList />
       )}
